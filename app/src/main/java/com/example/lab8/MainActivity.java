b5b550134dabd9b2ec516c900662c3b4e481b5b5
519000673TDTU <51900673@student.tdtu.vn>
2023-05-14 01:05:16 +0700

    bai 1

--- app/src/main/java/com/example/lab8/MainActivity.java
+++ app/src/main/java/com/example/lab8/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.lab8;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void getRequest(View view) {
+        this.startActivity(new Intent(this, GetRequestActivity.class));
+    }
+
+    public void postRequest(View view) {
+        this.startActivity(new Intent(this, PostRequestActivity.class));
+    }
+
 }
